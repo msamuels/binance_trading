a278ed4a219799ee276c2570389fdd825631c84d
Mark S <markspeed_718@yahoo.com>
2019-07-08 19:32:56 -0400

    make % stop loss and main coin configurable

--- app/logic/create_execute_buy.py
+++ app/logic/create_execute_buy.py
@@ -10,6 +10,8 @@
 
 from app.config import API_SECRET
 from app.config import API_KEY
+from app.config import STORE_OF_VALUE_COIN
+from app.config import PERCENT_STOP_LOSS
 
 from binance.client import Client
 from binance.enums import *
@@ -29,22 +31,22 @@ def main(symbol, price, qty):
     client = Client(API_KEY, API_SECRET)
 
     # 1  Get main token balance.
-    balance = get_asset_balance(client, 'USDT')
+    balance = binance_trade.get_asset_balance(client, STORE_OF_VALUE_COIN)
 
     # 2 Ensure balance > $10
-    if is_balance_enough(balance['free']):
+    if binance_trade.is_balance_enough(balance['free']):
 
         # 3 Calculate cost of purchase + trade fee
-        fee = get_trade_fee(client, symbol)
+        fee = binance_trade.get_trade_fee(client, symbol)
 
         # 4 Create and submit Buy order
         # 5 Get order ID
-        order = create_order(client, symbol, order_type=SIDE_BUY, quantity=qty, price=price)
+        order = binance_trade.create_order(client, symbol, order_type=SIDE_BUY, quantity=qty, price=price)
 
         # check if the order status
         if order['status'] == 'FILLED':
-            # 6 Submit Stop limit order 2% lower than purchase price
-            stop_limit_price = price - (price * .02)
-            stop_limit_order = create_order(client, symbol, order_type=ORDER_TYPE_STOP_LOSS_LIMIT,
-                                            quantity=qty, price=stop_limit_price)
+            # 6 Submit Stop limit order a % lower than purchase price
+            stop_limit_price = price - (price * PERCENT_STOP_LOSS)
+            stop_limit_order = binance_trade.create_order(client, symbol, order_type=ORDER_TYPE_STOP_LOSS_LIMIT,
+                                                            quantity=qty, price=stop_limit_price)
 
